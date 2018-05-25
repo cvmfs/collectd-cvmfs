5c7e4ccd9821491aec655e3ee6582770091757eb
Steve Traylen <steve.traylen@cern.ch>
2018-05-25 14:37:49 +0200

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '1.0.0'
+version = '1.0.1'
 
 install_requires = [
     'xattr',
