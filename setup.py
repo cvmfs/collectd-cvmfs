149e94c7bfd3411d51535cf984d434ec3fd4f5dd
Steve Traylen <steve@traylen.net>
2020-07-08 10:29:20 +0200

    Release 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '1.3.0'
+version = '1.3.1'
 
 install_requires = [
     'pyxattr',
